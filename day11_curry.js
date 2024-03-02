652e729b7177dee0b7a2120c47d9e5af3dd52fb5
Kaushal Chauhan <kaushalchauhan4199@gmail.com>
2024-03-02 14:03:47 +0530

    solved Problem curry, day 11

---
+++ day11_curry.js
@@ -0,0 +1,13 @@
+const curry = function (fn) {
+  let nums = [];
+  return function curried(...args) {
+    nums = [...nums, ...args];
+    if (fn.length === nums.length) {
+      const res = fn(...nums);
+      nums = [];
+      return res;
+    } else {
+      return curried;
+    }
+  };
+};
