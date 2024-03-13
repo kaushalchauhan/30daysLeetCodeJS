7183c2ddcc2f75f3019f621bc49ceccd9f5cb648
Kaushal Chauhan <kaushalchauhan4199@gmail.com>
2024-03-13 19:37:43 +0530

    solved Problem - Convert Object to JSON String(2633)

--- day17_OBJ2JSON.js
+++ day17_OBJ2JSON.js
@@ -1,3 +1,28 @@
 // Problem - Convert Object to JSON String(2633)
 
-const jsonStringify = function (object) {};
+const jsonStringify = function (object) {
+  // null , undefined
+  if (typeof object === "null" || typeof object === "undefined") {
+    return String(object);
+  }
+  // Arrays
+  if (Array.isArray(object)) {
+    const values = object.map((obj) => jsonStringify(obj));
+    return `[${values.join(",")}]`;
+  }
+  // Objects
+  if (typeof object === "object") {
+    const keys = Object.keys(object);
+    const keyValuePair = keys.map((key) => `"${key}" : ${jsonStringify(obj)}`);
+    return `{${keyValuePair.join(",")}}`;
+  }
+  // String
+  if (typeof object === "string") {
+    return `"${String(object)}"`;
+  }
+  return String(object);
+};
+
+const res = jsonStringify([1, 2, 3, 4, 5]);
+console.log(res);
+console.log(typeof res);
